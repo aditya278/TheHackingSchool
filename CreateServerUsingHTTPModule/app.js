981b6bfb5fb2f04deb48ed317d62050e138262c5
Aditya Shukla <aditya.shukla278@gmail.com>
2020-09-19 23:56:07 +0530

    Fixed the code

--- CreateServerUsingHTTPModule/app.js
+++ CreateServerUsingHTTPModule/app.js
@@ -10,7 +10,6 @@ const path = './CreateServerUsingHTTPModule/files';
 const writeFile = util.promisify(fs.writeFile);
 const readFile = util.promisify(fs.readFile);
 const appendFile = util.promisify(fs.appendFile);
-const access = util.promisify(fs.access);
 
 const server = http.createServer((req, res) => {
     if(req.url === '/favicon.ico') {
@@ -26,15 +25,13 @@ const server = http.createServer((req, res) => {
         let password = q.password;
         let userdata = q.userdata;
         const filePath = `${path}/${username}.txt`;
+        let date = new Date();
+        const out = `[${date.toLocaleString()}]: ${userdata}\n`;
 
         fs.access(filePath, (err) => {
 
             if(err) {
-                res.end("<h1>File Already Exist</h1>")
-            }
-            else {
-                let date = new Date();
-                const out = `[${date.toLocaleString()}]: ${userdata}`;
+                
                 const fileHeader = `UserName: ${username} Password: ${password}\n`;
             
                 writeFile(filePath, (fileHeader + out))
@@ -45,6 +42,18 @@ const server = http.createServer((req, res) => {
                     .catch((err) => {
                         res.statusCode = 500;
                         res.end("<h1> Data Save NOT Successfully </h1>");
+                    });
+            }
+            else {
+                
+                    appendFile(filePath, out)
+                    .then(() => {
+                        res.statusCode = 200;
+                        res.end("<h1> Data Appended to the file Successfully </h1>");
+                    })
+                    .catch((err) => {
+                        res.statusCode = 500;
+                        res.end("<h1> Data was NOT Appended to the file. </h1>");
                     });
             }
         });
